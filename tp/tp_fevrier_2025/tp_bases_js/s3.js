b00387a2451eb8416a42bfc5b75a69df4ade981f
didierDefrance <didier@d-defrance.fr>
2025-02-12 16:58:07 +0100

    s

--- tp/tp_fevrier_2025/tp_bases_js/s3.js
+++ tp/tp_fevrier_2025/tp_bases_js/s3.js
@@ -6,18 +6,19 @@ let taille=tab.length;
 //for(let i = 0; i<taille ;i++){
 for(let i = taille -1; i>=0 ; i--){
     if(tab[i].toLowerCase().startsWith('a')){
-        delete tab[i];
-        //tab.splice(i,1,/*rien*/);
+        //delete tab[i];
+        tab.splice(i,1/*,rien*/);
     }
 }
 
 
 //afficher le tableau avec les éléments qui restent en majuscule
 for(let i=0;i<tab.length;i++){
-    if(tab[i]!=undefined){
+    //console.log(`tab[${i}]=${tab[i]}`) 
+   if(tab[i]!=undefined){
         console.log(`tab[${i}]=${tab[i].toUpperCase()}`) 
         //plante si tab[i] vaut null ou undefined
-    }
+   }
 }
 console.log("------------")
 for(let j in tab){
