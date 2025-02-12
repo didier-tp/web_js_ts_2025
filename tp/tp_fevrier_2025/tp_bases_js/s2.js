f70f933b74e47b91363df3055bc5d26d526475ed
didierDefrance <didier@d-defrance.fr>
2025-02-12 15:55:45 +0100

    s

--- tp/tp_fevrier_2025/tp_bases_js/s2.js
+++ tp/tp_fevrier_2025/tp_bases_js/s2.js
@@ -12,18 +12,18 @@ let comparateurNombresCroissants = function(va,vb){
     */
    return va-vb;
 }
-tabNombres.sort(comparateurNombresCroissants);
-//tabNombres.sort((va,vb)=>va-vb); //tri par ordre croissant
+//tabNombres.sort(comparateurNombresCroissants);
+tabNombres.sort((va,vb)=>va-vb); //tri par ordre croissant
 
 
 //En Tp : trier ces tableaux par ordre croissant et décroissant
 //puis afficher les éléments d'une manière ou d'une autre 
 //boucle for( ...; i++) ou boucle for (... in ...) ou for(...of ...)
 /*
-for(let i=0;i<tabNoms.length;i++){
+for(let i=0;i<tabNombres.length;i++){
     console.log(`tabNombres[${i}]=${tabNombres[i]}`)
-}*/
-
+}
+*/
 for(let i in tabNombres){
     console.log(`tabNombres[${i}]=${tabNombres[i]}`)
 }
