45ec84dcd04a05b69fbc285c494e00b4eb83b576
didierDefrance <didier@d-defrance.fr>
2025-02-13 17:24:46 +0100

    s

--- tp/tp_fevrier_2025/tpJsWeb/js/devise.js
+++ tp/tp_fevrier_2025/tpJsWeb/js/devise.js
@@ -6,23 +6,35 @@ window.onload=function(){
 }
 
 function onAjout(){
+let spanMessage = document.getElementById("spanMessage");
+spanMessage.innerHTML="";
 // récupérer valeur saisies sous forme d'objet javascript
 let inputCode=document.querySelector("input[name=code]")
+//console.log(`code=${inputCode.value}`)
 let inputName=document.querySelector("input[name=name]")
 let inputChange=document.querySelector("input[name=change]")
-//console.log(`code=${inputCode.value}`)
+let changeSaisi = inputChange.value;
+//on vérifie via isNaN() si changeSaisi est numerique ou pas
+if(isNaN(changeSaisi)){
+    //si pas numerique on affiche (en rouge) un message d'erreur dans "spanMessage"
+    spanMessage.innerHTML="le taux de change n'est pas numerique";
+    spanMessage.style.color="red";
+    return; //sans executer fin de fonction
+}
+//si numerique on ajoute la ligne dans le tableau
+
 let objDevise = { code : inputCode.value ,
                   name : inputName.value,
-                  change : Number(inputChange.value) };
+                  change : Number(changeSaisi) };
 console.log(`objDevise=${JSON.stringify(objDevise)}`)
 
 // ajout des valeurs dans une nouvelle ligne du tableau
 let eltTBody = document.getElementById("tBody");
-var newRow = eltTBody.insertRow(-1) ; //new <tr> and .appendChild()
-var newCellCode = newRow.insertCell(0);  //new <td> and .appendChild()
+let newRow = eltTBody.insertRow(-1) ; //new <tr> and .appendChild()
+let newCellCode = newRow.insertCell(0);  //new <td> and .appendChild()
 newCellCode.innerText = objDevise.code;
-var newCellName = newRow.insertCell(1);  //new <td> and .appendChild()
+let newCellName = newRow.insertCell(1);  //new <td> and .appendChild()
 newCellName.innerText = objDevise.name;
-var newCellChange = newRow.insertCell(2);  //new <td> and .appendChild()
+let newCellChange = newRow.insertCell(2);  //new <td> and .appendChild()
 newCellChange.innerText = objDevise.change;
 }
