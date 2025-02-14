03ab4512aaf72db6550a9469b1fcb272c8e4ef71
didierDefrance <didier@d-defrance.fr>
2025-02-14 09:00:14 +0100

    s

---
+++ exemples/ex_ajax/js/compteAjax.js
@@ -0,0 +1,53 @@
+//************** SPECIF CRUD ********
+
+window.onload=function(){
+	(document.getElementById("inputId")).disabled=true; //if auto_incr
+	initListeners(); 
+}
+
+function objectFromInput(){
+	let numero = (document.getElementById("inputId")).value;
+	if(numero=="")numero=null;
+	
+	let label = (document.getElementById("inputLabel")).value;
+	let soldeInitial = (document.getElementById("inputSoldeInitial")).value;
+	
+	document.getElementById("spanMsg").innerHTML="";
+	let obj = { numero : numero,
+				label : label,
+	            solde : parseFloat(soldeInitial)
+	            };
+	return obj;
+}
+
+function displayObject(obj){
+	(document.getElementById("inputId")).value=obj.numero;
+	(document.getElementById("inputLabel")).value=obj.label;
+	(document.getElementById("inputSoldeInitial")).value=obj.solde;
+}
+
+function insertRowCells(row,obj){
+	(row.insertCell(0)).innerHTML = obj.numero;
+	(row.insertCell(1)).innerHTML = obj.label;
+	(row.insertCell(2)).innerHTML = obj.solde;
+}
+
+
+function blankObject(){
+	return {numero:"" , label: "" , solde : 0 };	
+}
+
+function getWsBaseUrl(){
+	return "./rest/api-bank/v1/comptes";
+}
+
+//obj = object with values to check
+//action = "add" or "update" or ...
+function canDoAction(action,obj){
+	ok=true; //by default
+	if(obj.label==null || obj.label == "")
+	  ok=false;
+	if(obj.solde==null)
+	  ok=false;
+	return ok;
+}
